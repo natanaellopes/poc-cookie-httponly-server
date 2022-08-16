a568dcef081058d0ca35db0af7188b275da56767
Natanael Lopes <natanael.lopes@nuvemshop.com.br>
2022-08-16 14:55:12 -0300

    feat: default values to localhost

---
+++ README.md
@@ -0,0 +1,20 @@
+
+# Instalação
+
+1. Clone o repositório do projeto
+```bash
+$ git clone https://github.com/natanaellopes/poc-cookie-httponly-server
+```
+2. Acesse a pasta criada
+```bash
+$ cd poc-cookie-httponly-server
+```
+3. Instale as dependências
+```bash
+$ yarn
+```
+4. Execute o comando:
+```bash
+$ yarn dev
+```
+5. Acesse a URL `http://localhost:3333`
--- src/actions/TokenAction.ts
+++ src/actions/TokenAction.ts
@@ -4,15 +4,24 @@ import { randomUUID } from 'crypto';
 const TokenAction = async (_req: Request, res: Response): Promise<any> => {
   const token = randomUUID();
 
+  const expires = new Date(Date.now() + (1000 * 60 * 60 * 2)); // 2 hours
+
   res.cookie('X-Token-Bearer', token, {
-    maxAge: 1000 * 60 * 15, // 15 min
+    expires,
     httpOnly: true,
     secure: true,
     sameSite: 'none',
     domain: 'localhost',
   });
 
-  return res.json({ token });
+  return res.json({ 
+    cookie_expires: expires, 
+    user: {
+      id: '1',
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+    }
+  });
 } 
 
 export { TokenAction };
